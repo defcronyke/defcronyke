691e2ad7d874dd53450eb561cc3e12127899e8b7
Jeremy Carter <jeremy@jeremycarter.ca>
2021-12-18 08:15:40 -0500

    Updates

--- precache-manifest.50a08fb6251cf1676227716f96d660d9.js
+++ precache-manifest.50a08fb6251cf1676227716f96d660d9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79bec435b722452fd3973b52ea27b001",
+    "revision": "fc284b25cbed5b54eea04c63818d0ef9",
     "url": "/index.html"
   },
   {
-    "revision": "5822ca108ff8a62a118c",
+    "revision": "341dbdaaee185117c46d",
     "url": "/static/css/main.a29d2fce.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d912daa0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5822ca108ff8a62a118c",
-    "url": "/static/js/main.5c57e721.chunk.js"
+    "revision": "341dbdaaee185117c46d",
+    "url": "/static/js/main.51d75575.chunk.js"
   },
   {
     "revision": "d733541c685ff77eb92f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dacbfd81bedb206cbbbbc011f90d772.js"
+  "/precache-manifest.50a08fb6251cf1676227716f96d660d9.js"
 );
 
 self.addEventListener('message', (event) => {
